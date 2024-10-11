b9e67c1c17eca494a9365c3c5368e568099c54a7
Daghmehchi <128915860+Daghmehchi@users.noreply.github.com>
2024-10-11 19:52:54 +0330

    Create Road_sign_detector.py

---
+++ training/Road_sign_detector.py
@@ -0,0 +1,34 @@
+%cd /content
+!git clone https://github.com/ultralytics/yolov5
+%cd yolov5
+!pip install -r requirements.txt
+!pip install kaggle
+!pip install numpy
+!python -m pip install --upgrade pip
+!pip install torch
+
+# Api for kaggle.json
+api_token = {
+    "username": "daghmehchi",
+    "key": "204cf422231f7a64709979b248e88822"
+}
+
+import json
+import os
+
+os.makedirs('/root/.kaggle/', exist_ok=True)
+
+with open('/root/.kaggle/kaggle.json', 'w') as file:
+    json.dump(api_token, file)
+
+# Access permission
+os.chmod('/root/.kaggle/kaggle.json', 600)
+
+# download from Kaggle
+!kaggle datasets download -d pkdarabi/cardetection
+
+# unzip
+!unzip cardetection.zip
+
+# train order
+!python train.py --img 416 --batch 16 --epochs 20 --data /content/car/data.yaml --weights yolov5s.pt --cache
