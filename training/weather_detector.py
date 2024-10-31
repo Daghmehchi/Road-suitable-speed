1d3851dc8543f304493df34aa4c6e038ed8244d0
Daghmehchi <128915860+Daghmehchi@users.noreply.github.com>
2024-10-31 12:19:20 +0000

    Create weather_detector.py

---
+++ training/weather_detector.py
@@ -0,0 +1,27 @@
+!pip install roboflow
+
+from roboflow import Roboflow
+rf = Roboflow(api_key="iybx5q0n2YlNOs0BUS83")
+project = rf.workspace("carlisence-gpfkw").project("weather_detection-v7zbi")
+version = project.version(1)
+dataset = version.download("folder")
+
+!pip install ultralytics
+%cd {HOME}
+!git clone github.com/ultralytics/ultralytics
+%cd {HOME}/ultralytics
+!pip install -e .
+
+#free space
+from IPython import display
+display.clear_output()
+
+#check requerments
+import ultralytics
+ultralytics.checks()
+
+from ultralytics import YOLO
+from IPython.display import display, Image
+
+# train order
+!yolo task=classify mode=train model=yolov8n-cls.pt data=/content/Weather_detection-1 epochs=50 imgsz=640
